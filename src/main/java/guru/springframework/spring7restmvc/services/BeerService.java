8ee73954b869fe4feeb0c9ea23ac0f77d03130e0
apg <carricksoft@gmail.com>
2026-04-25 17:36:19 +0100

    initial commit

---
+++ src/main/java/guru/springframework/spring7restmvc/services/BeerService.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring7restmvc.services;
+
+import guru.springframework.spring7restmvc.model.Beer;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+public interface BeerService {
+
+    List<Beer> listBeers();
+
+    Beer getBeerById(UUID id);
+}
