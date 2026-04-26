bf1eaa34fad059eb81eeb7e36d7d3768db3932dd
apg <carricksoft@gmail.com>
2026-04-26 17:06:55 +0100

    Initial personController getPersonByIdTest

--- src/test/java/scot/carricksoftware/grants2/controller/BeerControllerTest.java
+++ src/test/java/scot/carricksoftware/grants2/controller/BeerControllerTest.java
@@ -2,20 +2,31 @@ package scot.carricksoftware.grants2.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import scot.carricksoftware.grants2.services.PersonService;
 
 import java.util.UUID;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@SpringBootTest
+@WebMvcTest(PersonController.class)
 class BeerControllerTest {
 
     @Autowired
-    PersonController personController;
+    MockMvc mockMvc;
+
+    @MockitoBean
+    PersonService personService;
 
     @Test
-    void getPersonById() {
-
-        System.out.println(personController.getPersonById(UUID.randomUUID()));
-
+    void getPersonByIdTest() throws Exception {
+        mockMvc.perform(get("/people/" + UUID.randomUUID())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
