eb589b21e7a6cb9f9929a7c702cfc99eba89b503
apg <carricksoft@gmail.com>
2026-04-26 17:28:34 +0100

    Initial personController getPersonByIdTest improved

--- src/test/java/scot/carricksoftware/grants2/controller/BeerControllerTest.java
+++ src/test/java/scot/carricksoftware/grants2/controller/BeerControllerTest.java
@@ -6,14 +6,19 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import scot.carricksoftware.grants2.model.Person;
 import scot.carricksoftware.grants2.services.PersonService;
+import scot.carricksoftware.grants2.services.PersonServiceImpl;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@SpringBootTest
+
 @WebMvcTest(PersonController.class)
 class BeerControllerTest {
 
@@ -21,12 +26,18 @@ class BeerControllerTest {
     MockMvc mockMvc;
 
     @MockitoBean
-    PersonService personService;
+    PersonService personServiceMock;
+
+    final PersonService personService = new PersonServiceImpl();
 
     @Test
     void getPersonByIdTest() throws Exception {
+        Person testPerson = personService.listPeople().getFirst();
+        given(personServiceMock.getPersonById(any(UUID.class))).willReturn(testPerson);
+
         mockMvc.perform(get("/people/" + UUID.randomUUID())
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 }
