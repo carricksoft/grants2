d4cce1873c8b03d99ea5af0345bce12784fe9538
apg <carricksoft@gmail.com>
2026-04-28 22:49:52 +0100

    PersonControllerIT::personIsNotFoundTest

--- src/test/java/scot/carricksoftware/grants2/controller/PersonControllerIT.java
+++ src/test/java/scot/carricksoftware/grants2/controller/PersonControllerIT.java
@@ -9,12 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import scot.carricksoftware.grants2.entities.Person;
+import scot.carricksoftware.grants2.exceptions.NotFoundException;
 import scot.carricksoftware.grants2.model.PersonDTO;
 import scot.carricksoftware.grants2.repositories.PersonRepository;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
@@ -38,5 +42,18 @@ class PersonControllerIT {
         personRepository.deleteAll();
         List<PersonDTO> dtos = personController.listPeople();
         assertThat(dtos.size()).isEqualTo(0);
+    }
+
+    @Test
+    void getByIdTest() {
+        Person person = personRepository.findAll().getFirst();
+        PersonDTO dto = personController.getPersonById(person.getId());
+
+        assertThat(dto).isNotNull();
+    }
+
+    @Test
+    void personIsNotFoundTest(){
+        assertThrows(NotFoundException.class, () -> personController.getPersonById(UUID.randomUUID()));
     }
 }
