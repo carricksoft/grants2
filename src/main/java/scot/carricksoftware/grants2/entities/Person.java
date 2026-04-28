f50931feea0577d9769ab0ef7a74f8ece87b15ab
apg <carricksoft@gmail.com>
2026-04-28 09:51:08 +0100

    UUID Generator

--- src/main/java/scot/carricksoftware/grants2/entities/Person.java
+++ src/main/java/scot/carricksoftware/grants2/entities/Person.java
@@ -4,7 +4,9 @@
 
 package scot.carricksoftware.grants2.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
@@ -12,6 +14,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -23,8 +26,13 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
+
     @Id
+    @GeneratedValue(generator="UUID")
+    @UuidGenerator
+    @Column(length=36, columnDefinition="varchar", updatable=false, nullable=false)
     private UUID id;
+
     @Version
     private Integer version;
 
