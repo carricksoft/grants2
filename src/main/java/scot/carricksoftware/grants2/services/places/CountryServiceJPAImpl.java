f0191f2de4cb5e6bd33e767c5cddf84f5459aefd
apg <carricksoft@gmail.com>
2026-05-01 18:48:10 +0100

    CountryServiceJPAImpl

---
+++ src/main/java/scot/carricksoftware/grants2/services/places/CountryServiceJPAImpl.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2026.  Andrew Grant, Carrick Software. All rights reserved
+ */
+
+package scot.carricksoftware.grants2.services.places;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import scot.carricksoftware.grants2.mappers.places.CountryMapper;
+import scot.carricksoftware.grants2.model.places.CountryDTO;
+import scot.carricksoftware.grants2.repositories.places.CountryRepository;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CountryServiceJPAImpl implements CountryService {
+
+    private final CountryRepository countryRepository;
+    private final CountryMapper countryMapper;
+
+    @Override
+    public List<CountryDTO> listCountries() {
+        return countryRepository.findAll()
+                .stream()
+                .map(countryMapper::countryToCountryDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<CountryDTO> getCountryById(UUID id) {
+        return Optional.ofNullable(countryMapper
+                .countryToCountryDto(countryRepository
+                .findById(id)
+                .orElse(null)));
+    }
+
+    @Override
+    public CountryDTO saveNewCountry(CountryDTO countryDTO) {
+        return countryMapper.countryToCountryDto(countryRepository.save(countryMapper.countryDtoToCountry(countryDTO)));
+    }
+
+    @Override
+    public Optional<CountryDTO> updateCountryById(UUID id, CountryDTO countryDTO) {
+        AtomicReference<Optional<CountryDTO>> atomicReference = new AtomicReference<>();
+
+        countryRepository.findById(id).ifPresentOrElse(foundCountry -> {
+            foundCountry.setName(countryDTO.getName());
+            foundCountry.setUpdatedDate(LocalDateTime.now());
+            atomicReference.set(Optional.of(countryMapper
+                    .countryToCountryDto(countryRepository.save(foundCountry))));
+        }, () -> atomicReference.set(Optional.empty()));
+
+        return atomicReference.get();
+    }
+
+    @Override
+    public Boolean deleteCountryById(UUID id) {
+        if (countryRepository.existsById(id)) {
+            countryRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+}
