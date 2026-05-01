4493d94c29be88513fd0342155a330a1627105e5
apg <carricksoft@gmail.com>
2026-05-01 13:35:34 +0100

    Added Country entity

---
+++ src/main/java/scot/carricksoftware/grants2/entities/places/Country.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2026.  Andrew Grant, Carrick Software. All rights reserved
+ */
+
+package scot.carricksoftware.grants2.entities.places;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import scot.carricksoftware.grants2.entities.BaseEntity;
+
+@Getter
+@Setter
+@SuperBuilder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Country extends BaseEntity {
+
+
+    @NotNull
+    @NotBlank
+    @Size(max=FIELD_SIZE)
+    @Column(length=128)
+    private String name;
+
+}
