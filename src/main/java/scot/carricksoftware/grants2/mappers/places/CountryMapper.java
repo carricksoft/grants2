5f4809a737df40856d00dcb65e7f09da2777c24f
apg <carricksoft@gmail.com>
2026-05-01 13:41:29 +0100

    Added Country mapper

---
+++ src/main/java/scot/carricksoftware/grants2/mappers/places/CountryMapper.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2026.  Andrew Grant, Carrick Software. All rights reserved
+ */
+
+package scot.carricksoftware.grants2.mappers.places;
+
+import org.mapstruct.Mapper;
+import scot.carricksoftware.grants2.entities.places.Country;
+import scot.carricksoftware.grants2.model.places.CountryDTO;
+
+@Mapper
+public interface CountryMapper {
+
+    Country countryDtoToCountry(CountryDTO dto);
+    CountryDTO countryToCountryDto(Country country);
+}
