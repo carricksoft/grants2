ffe87b0540fb24c846a061acd563fb38838996da
apg <carricksoft@gmail.com>
2026-05-02 10:02:23 +0100

    Place Bootstrap added

---
+++ src/main/java/scot/carricksoftware/grants2/bootstrap/BootstrapPerson.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2026.  Andrew Grant, Carrick Software. All rights reserved
+ */
+
+package scot.carricksoftware.grants2.bootstrap;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import scot.carricksoftware.grants2.entities.Person;
+import scot.carricksoftware.grants2.repositories.PersonRepository;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+public class BootstrapPerson implements CommandLineRunner {
+
+    private final PersonRepository personRepository;
+
+    @Override
+    public void run(@SuppressWarnings("NullableProblems") String... args) {
+        loadPersonData();
+    }
+
+    private void loadPersonData() {
+        if (personRepository.count() == 0) {
+            Person person1 = Person.builder()
+                    .firstName("Person 1F")
+                    .lastName("Person 1L")
+                    .version(1)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
+
+            Person person2 = Person.builder()
+                    .firstName("Person 2F")
+                    .lastName("Person 2L")
+                    .version(1)
+                    .certifiedYearOfBirth("1953")
+                    .certifiedYearOfDeath("2050")
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
+
+            Person person3 = Person.builder()
+                    .firstName("Person 3F")
+                    .lastName("Person 3L")
+                    .recordedYearOfBirth("1955")
+                    .version(1)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
+
+            personRepository.saveAll(Arrays.asList(person1, person2, person3));
+        }
+
+    }
+
+}
--- src/main/java/scot/carricksoftware/grants2/bootstrap/BootstrapPlaces.java
+++ src/main/java/scot/carricksoftware/grants2/bootstrap/BootstrapPlaces.java
@@ -8,11 +8,11 @@ package scot.carricksoftware.grants2.bootstrap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import scot.carricksoftware.grants2.entities.Person;
 import scot.carricksoftware.grants2.entities.places.Country;
+import scot.carricksoftware.grants2.entities.places.Place;
 import scot.carricksoftware.grants2.entities.places.Region;
-import scot.carricksoftware.grants2.repositories.PersonRepository;
 import scot.carricksoftware.grants2.repositories.places.CountryRepository;
+import scot.carricksoftware.grants2.repositories.places.PlaceRepository;
 import scot.carricksoftware.grants2.repositories.places.RegionRepository;
 
 import java.time.LocalDateTime;
@@ -20,52 +20,19 @@ import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
-public class BootstrapData implements CommandLineRunner {
+public class BootstrapPlaces implements CommandLineRunner {
 
-    private final PersonRepository personRepository;
     private final CountryRepository countryRepository;
     private final RegionRepository regionRepository;
+    private final PlaceRepository placeRepository;
 
     @Override
     public void run(@SuppressWarnings("NullableProblems") String... args) {
-        loadPersonData();
         loadCountryData();
         loadRegionData();
+        loadPlaceData();
     }
-    
-    private void loadPersonData() {
-        if (personRepository.count() == 0) {
-            Person person1 = Person.builder()
-                    .firstName("Person 1F")
-                    .lastName("Person 1L")
-                    .version(1)
-                    .createdDate(LocalDateTime.now())
-                    .updatedDate(LocalDateTime.now())
-                    .build();
 
-            Person person2 = Person.builder()
-                    .firstName("Person 2F")
-                    .lastName("Person 2L")
-                    .version(1)
-                    .certifiedYearOfBirth("1953")
-                    .certifiedYearOfDeath("2050")
-                    .createdDate(LocalDateTime.now())
-                    .updatedDate(LocalDateTime.now())
-                    .build();
-
-            Person person3 = Person.builder()
-                    .firstName("Person 3F")
-                    .lastName("Person 3L")
-                    .recordedYearOfBirth("1955")
-                    .version(1)
-                    .createdDate(LocalDateTime.now())
-                    .updatedDate(LocalDateTime.now())
-                    .build();
-
-            personRepository.saveAll(Arrays.asList(person1, person2, person3));
-        }
-
-    }
 
     private void loadCountryData() {
         if (countryRepository.count() == 0) {
@@ -89,7 +56,6 @@ public class BootstrapData implements CommandLineRunner {
                     .createdDate(LocalDateTime.now())
                     .updatedDate(LocalDateTime.now())
                     .build();
-
 
 
             countryRepository.saveAll(Arrays.asList(country1, country2, country3));
@@ -125,5 +91,33 @@ public class BootstrapData implements CommandLineRunner {
         }
     }
 
+    private void loadPlaceData() {
+        if (placeRepository.count() == 0) {
+            Place place1 = Place.builder()
+                    .name("Place 1")
+                    .version(1)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
 
+            Place place2 = Place.builder()
+                    .name("Place 2")
+                    .version(1)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
+
+            Place place3 = Place.builder()
+                    .name("Place 3")
+                    .version(1)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
+
+
+            placeRepository.saveAll(Arrays.asList(place1, place2, place3));
+        }
     }
+
+
+}
