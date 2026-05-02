1fedd330a4bafb18fdb6f03c36f5e6aab686bbb8
apg <carricksoft@gmail.com>
2026-05-02 01:03:35 +0100

    Region entity and DTO

---
+++ src/main/java/scot/carricksoftware/grants2/model/places/RegionDTO.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2026.  Andrew Grant, Carrick Software. All rights reserved
+ */
+
+package scot.carricksoftware.grants2.model.places;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import scot.carricksoftware.grants2.model.BaseEntityDTO;
+
+
+@SuperBuilder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegionDTO extends BaseEntityDTO {
+
+    @NotBlank
+    @NotNull
+    @JsonProperty("name")
+    private String name;
+
+
+}
