4e7035b012b3cda9186aabd01bf00118f046d9e1
apg <carricksoft@gmail.com>
2026-05-03 09:59:11 +0100

    PlaceRepositoryTest

--- src/main/java/scot/carricksoftware/grants2/repositories/places/PlaceRepository.java
+++ src/main/java/scot/carricksoftware/grants2/repositories/places/PlaceRepository.java
@@ -7,8 +7,10 @@ package scot.carricksoftware.grants2.repositories.places;
 import org.springframework.data.jpa.repository.JpaRepository;
 import scot.carricksoftware.grants2.entities.places.Place;
 
+import java.util.List;
 import java.util.UUID;
 
 @SuppressWarnings("unused")
 public interface PlaceRepository extends JpaRepository<Place, UUID> {
+    List<Place> findAllByNameIsLikeIgnoreCase(String place);
 }
---
+++ src/test/java/scot/carricksoftware/grants2/repositories/PlaceRepositoryTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2026.  Andrew Grant, Carrick Software. All rights reserved
+ */
+
+package scot.carricksoftware.grants2.repositories;
+
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import scot.carricksoftware.grants2.bootstrap.BootstrapPlaces;
+import scot.carricksoftware.grants2.entities.places.Place;
+import scot.carricksoftware.grants2.repositories.places.PlaceRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static scot.carricksoftware.grants2.entities.Person.FIELD_SIZE;
+
+
+@DataJpaTest
+@Import(BootstrapPlaces.class)
+class PlaceRepositoryTest {
+
+    @Autowired
+    PlaceRepository placeRepository;
+
+    @Test
+    void saveTest() {
+        Place savedPlace = placeRepository.save(Place.builder()
+                .name("Wales")
+                .build());
+        placeRepository.flush();
+
+        assertThat(savedPlace).isNotNull();
+        assertThat(savedPlace.getId()).isNotNull();
+        assertThat(savedPlace.getName()).isEqualTo("Wales");
+    }
+
+    @SuppressWarnings("unused")
+    @Test
+    void nameTooLongTest() {
+        String testString = longString(FIELD_SIZE + 1);
+        assertThrows(ConstraintViolationException.class, ()-> {
+            Place savedPlace = placeRepository.save(Place.builder()
+                    .name(testString)
+                    .build());
+            placeRepository.flush();
+        });
+    }
+
+    private String longString(@SuppressWarnings("SameParameterValue") int length) {
+        StringBuilder output = new StringBuilder("1234567890");
+        while (output.length() < length) {
+            output.append(output);
+        }
+        return output.substring(0, length);
+    }
+
+    @Test
+    void getPlaceByNameTest() {
+        List<Place> list = placeRepository.findAllByNameIsLikeIgnoreCase("%place 1%");
+        assertThat(list.size()).isEqualTo(1);
+    }
+
+
+}
+
